499cfd580eb64740077eadf2b4081f9db4208751
Shri <76138254+Shrinivas2708@users.noreply.github.com>
2025-08-30 13:49:24 +0530

    Update exports.ts

--- apps/client/src/lib/exports.ts
+++ apps/client/src/lib/exports.ts
@@ -1,6 +1,6 @@
 import  Icon from "/logo.svg"
 import User from "/user.svg"
-const backend_url = "https://uptimepulse-2nrq.onrender.com"
+const backend_url = "https://uptimepulse-0jpe.onrender.com/"
 // const backend_url = "http://localhost:3000"
 // const STATUS_PAGE_URL = "http://localhost:5174"
 const STATUS_PAGE_URL = "https://status.uptimepulse.shriii.xyz"
