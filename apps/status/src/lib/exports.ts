5c395d19053dca78db4c743214ebc3eb49b87c20
Shri <76138254+Shrinivas2708@users.noreply.github.com>
2025-08-30 13:50:02 +0530

    Update exports.ts

--- apps/status/src/lib/exports.ts
+++ apps/status/src/lib/exports.ts
@@ -1,5 +1,5 @@
 import  Icon from "/logo.svg"
 
-const backend_url = "https://uptimepulse-2nrq.onrender.com"
+const backend_url = "https://uptimepulse-0jpe.onrender.com"
 // const backend_url = "http://localhost:3000"
 export {Icon,backend_url}
